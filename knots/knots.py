e0cbb6e1049d099117c72a199496d428606f7dae
Isaac Beh <isaacwbeh@gmail.com>
2025-01-23 18:36:46 +1100

    Added switching strands for Skien relations

--- knots/knots.py
+++ knots/knots.py
@@ -12,6 +12,13 @@ Loop = None
 Segment = Edge | Loop
 
 
+def change_edge(edge: Segment, min: int, amount: int):
+    if not edge:
+        return edge
+    f = lambda e: e + amount if e > min else e
+    return (f(edge[0]), f(edge[1]))
+
+
 class TransverseCrossing:
     """
         out-under   out-over
@@ -33,6 +40,24 @@ class TransverseCrossing:
         self.in_under = in_under
         self.in_over = in_over
         self.positive = positive
+
+    def __repr__(self) -> str:
+        if self.positive:
+            return f"""
+                       {self.out_under}   {self.out_over}
+                        \\ /
+                         /
+                        / \\
+                       {self.in_over}  {self.in_under}
+            """
+        else:
+            return f"""
+                           {self.out_under}   {self.out_over}
+                            \\ /
+                             \\
+                            / \\
+                           {self.in_over}  {self.in_under}
+                """
 
     def has_entry(self, edge: int) -> bool:
         """Don't allow for double entry"""
@@ -59,8 +84,31 @@ class TransverseCrossing:
         else:
             raise ValueError("Edge doesn't enter crossing")
 
+    def change_edge_indexes(self, min: int, amount: int) -> TransverseCrossing:
+        """Increase all edge indexes strictly above min by amount"""
+        change_strand = lambda strand: strand + amount if strand > min else strand
+        return TransverseCrossing(
+            change_strand(self.out_under),
+            change_strand(self.out_over),
+            change_strand(self.in_under),
+            change_strand(self.in_over),
+            self.positive,
+        )
 
-class SinglularCrossing:
+    def replace_input(self, old_edge: int, new_edge: int) -> TransverseCrossing:
+        if self.in_under == old_edge:
+            return TransverseCrossing(
+                self.out_under, self.out_over, new_edge, self.in_over, self.positive
+            )
+        elif self.in_over == old_edge:
+            return TransverseCrossing(
+                self.out_under, self.out_over, self.in_under, new_edge, self.positive
+            )
+        else:
+            raise ValueError("Cannot replace edge that doesn't exist")
+
+
+class SingularCrossing:
     """
         Written such that:
         exiting[0]   exiting[1]
@@ -80,6 +128,15 @@ class SinglularCrossing:
             raise ValueError("Incorrent number of exiting strands")
         self.entering = list(entering)
         self.exiting = list(exiting)
+
+    def __repr__(self) -> str:
+        return f"""
+                   {self.exiting[0]}   {self.exiting[1]}
+                    \\ /
+                     o
+                    / \\
+                   {self.entering[0]}  {self.entering[1]}
+        """
 
     def has_entry(self, edge: int) -> bool:
         return edge in self.entering and self.entering != [edge, edge]
@@ -106,8 +163,16 @@ class SinglularCrossing:
             self.exiting[0], self.exiting[1], self.entering[0], self.entering[1], False
         )
 
+    def change_edge_indexes(self, min: int, amount: int) -> SingularCrossing:
+        """Increase all edge indexes strictly above min by amount"""
+        change_strand = lambda strand: strand + amount if strand > min else strand
+        return SingularCrossing(
+            [change_strand(e) for e in self.entering],
+            [change_strand(e) for e in self.exiting],
+        )
 
-Crossing = TransverseCrossing | SinglularCrossing
+
+Crossing = TransverseCrossing | SingularCrossing
 
 
 class Knot:
@@ -150,11 +215,80 @@ class Knot:
         self.edges = edges
 
     def is_singular(self) -> bool:
-        return not any(isinstance(c, SinglularCrossing) for c in self.crossings)
+        return not any(isinstance(c, SingularCrossing) for c in self.crossings)
+
+    def swap_crossing(self, index: int) -> Knot:
+        crossing = self.crossings[index]
+        new_crossings = list(self.crossings)
+        if isinstance(crossing, SingularCrossing):
+            raise ValueError("You cannot swap a singular crossing")
+        if crossing.positive:
+            new_crossings[index] = TransverseCrossing(
+                crossing.out_over,
+                crossing.out_under,
+                crossing.in_over,
+                crossing.in_under,
+                False,
+            )
+        else:
+            new_crossings[index] = TransverseCrossing(
+                crossing.out_over,
+                crossing.out_under,
+                crossing.in_over,
+                crossing.in_under,
+                True,
+            )
+        return Knot(new_crossings, list(self.edges))
+
+    def untwist_crossing(self, index: int) -> Knot:
+        crossing = self.crossings[index]
+        new_crossings = list(self.crossings)
+        new_edges = list(self.edges)
+        if isinstance(crossing, SingularCrossing):
+            raise ValueError("You cannot untwist a singular crossing")
+
+        # TODO Do you need separate cases for left and right
+        if crossing.out_under == crossing.in_over:
+            new_edges[crossing.out_under] = None
+        else:
+            left_in_edge = new_edges[crossing.in_over]
+            left_out_edge = new_edges[crossing.out_under]
+            new_edges[crossing.in_over] = (left_in_edge[0], left_out_edge[1])
+            new_crossings[left_out_edge[1]] = new_crossings[
+                left_out_edge[1]
+            ].replace_input(crossing.out_under, crossing.in_over)
+
+            new_edges.pop(crossing.out_under)
+            new_crossings = [
+                c.change_edge_indexes(crossing.out_under, -1) for c in new_crossings
+            ]
+
+        # Update the crossing
+        crossing = new_crossings[index]
+
+        if crossing.out_over == crossing.in_under:
+            new_edges[crossing.out_over] = None
+        else:
+            right_in_edge = new_edges[crossing.in_under]
+            right_out_edge = new_edges[crossing.out_over]
+            new_edges[crossing.in_under] = (right_in_edge[0], right_out_edge[1])
+            new_crossings[right_out_edge[1]] = new_crossings[
+                right_out_edge[1]
+            ].replace_input(crossing.out_over, crossing.in_under)
+
+            new_edges.pop(crossing.out_over)
+            new_crossings = [
+                c.change_edge_indexes(crossing.out_over, -1) for c in new_crossings
+            ]
+
+        new_crossings.pop(index)
+        new_edges = [change_edge(e, index, -1) for e in new_edges]
+
+        return Knot(new_crossings, new_edges)
 
     def split_singular_crossings(self) -> tuple[list[Knot], list[Knot]]:
         for i, crossing in enumerate(self.crossings):
-            if isinstance(crossing, SinglularCrossing):
+            if isinstance(crossing, SingularCrossing):
                 positive_version = list(self.crossings)
                 positive_version[i] = crossing.positive_version()
                 negative_version = list(self.crossings)
@@ -176,7 +310,7 @@ class Knot:
         if not current_edge:
             raise ValueError("Linked to loop")
         crossing = self.crossings[current_edge[1]]
-        if isinstance(crossing, SinglularCrossing):
+        if isinstance(crossing, SingularCrossing):
             raise ValueError("A singular knot cannot be tranversed")
         return crossing.next_edge(current_index)
 
@@ -186,17 +320,18 @@ class Knot:
         while len(visited) < len(self.edges):
             component_crossings: set[int] = set()
             component_edges: set[int] = set()
-            current_index, current_edge = [
-                (i, e) for i, e in enumerate(self.edges) if e not in visited
-            ][0]
+            current_index = [i for i, _ in enumerate(self.edges) if i not in visited][0]
             while current_index not in visited:
                 component_edges.add(current_index)
+                visited.add(current_index)
+                current_edge = self.edges[current_index]
+
                 if not current_edge:
                     break
 
                 component_crossings.add(current_edge[1])
                 crossing = self.crossings[current_edge[1]]
-                if isinstance(crossing, SinglularCrossing):
+                if isinstance(crossing, SingularCrossing):
                     raise ValueError(
                         "Components can only be found in non-singular knots"
                     )
@@ -206,12 +341,47 @@ class Knot:
             result.append((component_crossings, component_edges))
         return result
 
+    def disjoint_sum(self, k: Knot):
+        new_crossings = self.crossings + [
+            c.change_edge_indexes(-1, len(self.edges)) for c in k.crossings
+        ]
+        new_edges = self.edges + [
+            change_edge(e, -1, len(self.crossings)) for e in k.edges
+        ]
+        return Knot(new_crossings, new_edges)
+
+    def remove_component(self, component: tuple[set[int], set[int]]):
+        new_crossings = list(self.crossings)
+        new_edges = list(self.edges)
+
+        seen: set[int] = set()
+        for crossing_index in component[0]:
+            seen.add(crossing_index)
+            crossing_index -= len([i for i in seen if i < crossing_index])
+            new_edges = [change_edge(e, crossing_index, -1) for e in new_edges]
+
+        seen: set[int] = set()
+        for edge_index in component[1]:
+            seen.add(edge_index)
+            edge_index -= len([i for i in seen if i < edge_index])
+            new_crossings = [
+                c.change_edge_indexes(edge_index, -1) for c in new_crossings
+            ]
+
+        for crossing_index in sorted(list(component[0]), reverse=True):
+            new_crossings.pop(crossing_index)
+        for edge_index in sorted(list(component[1]), reverse=True):
+            new_edges.pop(edge_index)
+
+        print(new_crossings, new_edges)
+        return Knot(new_crossings, new_edges)
+
     def draw(self):
         g = nx.MultiDiGraph()
         for i, crossing in enumerate(self.crossings):
             if isinstance(crossing, TransverseCrossing):
                 g.add_node(i, type="Transverse", crossing_directions=crossing.strands())
-            elif isinstance(crossing, SinglularCrossing):
+            elif isinstance(crossing, SingularCrossing):
                 g.add_node(i, type="Singular")
             else:
                 raise ValueError("Unknown crossing type")
