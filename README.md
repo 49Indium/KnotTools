532b26715f9ec4c20d780fe930ee5f87e9776e55
Isaac Beh <isaacwbeh@gmail.com>
2025-01-24 21:53:54 +1100

    Added more examples and updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-A rough package meant to help study knots and their invariants. Created for the AMSI2024 summer school for interest sake. Documentation and examples coming in the next few days.
+# Knot Tools
+
+A python library to help you study knots and calculate their invariants.
+
+Allows you to represent knots, links and braids (tangles coming soon), combine them together and calculate their invariants. Created during whilst studying the Algebraic Knot Theory course at the [2025 AMSI Summer School](https://ss.amsi.org.au/) to gain a better understanding and help computations. More documentation and features coming in the next few days as I add more to it.
+
+# Example Usage
+
+```python
+import knots.knots as *
+
+# A bunch of knots are already included
+print(trefoil.jones_polynomial())
+# Returns 1t^1 + 1t^3 + -1t^4
+
+# An example knot from one of the assignments
+# Knots consist of a list of crossings, each with the corresponding edges attached, and whether they are positive or negative. 
+# They also contain a list of edges, to help determine when crossings are connected.
+assignment_knot = Knot(
+    [
+        TransverseCrossing(5, 0, 4, 9, False),
+        TransverseCrossing(1, 6, 0, 5, False),
+        TransverseCrossing(7, 4, 6, 3, False),
+        TransverseCrossing(3, 8, 2, 7, False),
+        TransverseCrossing(9, 2, 8, 1, False) # A negative crossing
+    ],
+    edges_from_sequence([0, 1, 4, 3, 2, 0, 1, 2, 3, 4, 0])
+)
+
+print(assignment_knot.jones_polynomial().modified_polynomial_coefficient(4))
+# Returns (-65, 2), which represents -65/2
+
+# One of the knots equivalent to the chord diagram corresponding to ABCDCBAD
+# Crossings can be singular or transverse
+chord_diagram_example = Knot(
+    [
+        SingularCrossing([9, 4], [0, 5]),
+        SingularCrossing([7, 0], [8, 1]),
+        SingularCrossing([1, 6], [2, 7]),
+        SingularCrossing([5, 2], [6, 3]),
+        TransverseCrossing(4, 9, 3, 8, True) # A positive crossing
+    ],
+    edges_from_sequence([0, 1, 2, 3, 4, 0, 3, 2, 1, 4, 0])
+)
+
+print(assignment_knot.jones_polynomial())
+# Returns -1t^-2 + 1t^-1 + 2t^0 + -1t^1 + -2t^2 + -1t^3 + 2t^4 + 1t^5 + -1t^6
+
+```
--- src/knots/knots.py
+++ src/knots/knots.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
+from types import TracebackType
 from typing import Literal
 from itertools import zip_longest
 
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+import math
 
 VisitStatus = Literal["Half", "Visited"]
 # (From, to)
@@ -12,6 +14,23 @@ Edge = tuple[int, int]
 Loop = None
 Segment = Edge | Loop
 
+Fraction = tuple[int, int]
+
+def edges_from_sequence(crossings: list[int]):
+    prev = crossings[0]
+    result = []
+    for a in crossings[1:]:
+        result.append((prev,a))
+        prev = a
+    return result
+
+def reduce_fraction(frac: Fraction) -> Fraction:
+    n = 2
+    while n <= min(abs(frac[0]), abs(frac[1])):
+        if frac[0] % n == 0 and frac[1] % n == 0:
+            return reduce_fraction((frac[0]//n, frac[1]//n))
+        n += 1
+    return frac
 
 class Polynomial:
     lowest_power: int
@@ -25,12 +44,25 @@ class Polynomial:
         n = self.lowest_power
         r = ""
         for t in self.terms:
-            if n % 2 == 0:
-                r += f" + {t}t^{n // 2}"
-            else:
-                r += f" + {t}t^{n}/2"
+            if t != 0:
+                if n % 2 == 0:
+                    r += f" + {t}t^{n // 2}"
+                else:
+                    r += f" + {t}t^{n}/2"
             n += 1
         return r
+
+    def modified_polynomial_coefficient(self, i) -> int | Fraction:
+        n = self.lowest_power
+        r = 0
+        for t in self.terms:
+            if n != 0 and n % 2 == 0:
+                r += t * ((n//2)**i)
+            n += 1
+
+        if r % math.factorial(i) != 0:
+            return reduce_fraction((r, math.factorial(i)))
+        return r // math.factorial(i)
 
     def set_lowest_power(self, i: int):
         n = max(0, self.lowest_power - i)
@@ -45,25 +77,24 @@ class Polynomial:
             self.terms.pop(0)
 
     def add(self, p: Polynomial):
-        self.set_lowest_power(p.lowest_power)
-        self.terms = [
+        terms = [
             a + b
             for a, b in zip_longest(
-                self.terms,
+                [0] * max(0, self.lowest_power - p.lowest_power) + self.terms,
                 [0] * max(0, p.lowest_power - self.lowest_power) + p.terms,
                 fillvalue=0,
             )
         ]
-        self.reduce_zero_terms()
+        return Polynomial(min(self.lowest_power, p.lowest_power), terms)
 
     def scale(self, k: int):
         return Polynomial(self.lowest_power, [k * t for t in self.terms])
 
     def subtract(self, p: Polynomial):
-        self.add(p.scale(-1))
+        return self.add(p.scale(-1))
 
     def multiply_by_power(self, n: int):
-        return Polynomial(self.lowest_power + n, self.terms)
+        return Polynomial(self.lowest_power + n, list(self.terms))
 
 
 def change_edge(edge: Segment, min: int, amount: int):
@@ -256,7 +287,7 @@ class Knot:
                     raise ValueError("Crossing has an incoming loop")
                 if not crossings[entering_strand[0]].has_exit(entering_strand_index):
                     raise ValueError(
-                        f"Crossing number {i} has an ingoing strand {entering_strand}, but this strand does not enter crossing {entering_strand[0]}."
+                        f"Crossing number {i} has an ingoing strand {entering_strand} (index {entering_strand_index}), but this strand does not exit crossing {entering_strand[0]}."
                     )
 
         for i, edge in enumerate(edges):
@@ -474,15 +505,9 @@ class Knot:
             pos, neg = self.split_singular_crossings()
             p = Polynomial(0, [])
             for k in pos:
-                print(
-                    f"Calculated pos {k.crossings} {k.edges} to be {k.jones_polynomial()}"
-                )
-                p.add(k.jones_polynomial())
+                p = p.add(k.jones_polynomial())
             for k in neg:
-                print(
-                    f"Calculated neg {k.crossings} {k.edges} to be {k.jones_polynomial()}"
-                )
-                p.subtract(k.jones_polynomial())
+                p = p.subtract(k.jones_polynomial())
             return p
 
         if not self.edges:
@@ -495,8 +520,8 @@ class Knot:
             k = self.remove_component((set(), {0}))
             p = Polynomial(0, [])
             q = k.jones_polynomial()
-            p.subtract(q.multiply_by_power(1))
-            p.subtract(q.multiply_by_power(-1))
+            p = p.subtract(q.multiply_by_power(1))
+            p = p.subtract(q.multiply_by_power(-1))
             return p
 
         seen_edges: set[int] = set()
@@ -514,21 +539,21 @@ class Knot:
             elif self.crossings[current_crossing].positive:
                 p = Polynomial(0, [])
                 r = self.swap_crossing(current_crossing).jones_polynomial()
-                p.add(r.multiply_by_power(4))
+                p = p.add(r.multiply_by_power(4))
                 q = self.untwist_crossing(current_crossing).jones_polynomial()
-                p.add(q.multiply_by_power(3))
-                p.subtract(q.multiply_by_power(1))
+                p = p.add(q.multiply_by_power(3))
+                p = p.subtract(q.multiply_by_power(1))
                 return p
             else:
                 p = Polynomial(0, [])
-                p.add(
+                p = p.add(
                     self.swap_crossing(current_crossing)
                     .jones_polynomial()
                     .multiply_by_power(-4)
                 )
                 q = self.untwist_crossing(current_crossing).jones_polynomial()
-                p.add(q.multiply_by_power(-3))
-                p.subtract(q.multiply_by_power(-1))
+                p = p.add(q.multiply_by_power(-3))
+                p = p.subtract(q.multiply_by_power(-1))
                 return p
 
         k = self.remove_edges(list(seen_edges))
@@ -536,8 +561,8 @@ class Knot:
             return Polynomial(0, [1])
         p = Polynomial(0, [])
         q = k.jones_polynomial()
-        p.subtract(q.multiply_by_power(1))
-        p.subtract(q.multiply_by_power(-1))
+        p = p.subtract(q.multiply_by_power(1))
+        p = p.subtract(q.multiply_by_power(-1))
         return p
 
     def draw(self):
@@ -621,6 +646,97 @@ chord_3b = Knot(
     ],
     [(0, 1), (1, 2), (2, 3), (3, 1), (1, 0), (0, 3), (3, 2), (2, 0)],
 )
+chord_3a = Knot(
+    [
+        SingularCrossing([2,5],[3,0]),
+        SingularCrossing([0,3],[1,4]),
+        SingularCrossing([4,1],[5,2])
+    ],
+    [(0,1),(1,2),(2,0),(0,1),(1,2),(2,0)]
+)
+chord_3av2 = Knot(
+    [
+        SingularCrossing([2,5],[3,0]),
+        SingularCrossing([0,3],[1,4]),
+        SingularCrossing([4,1],[5,2])
+    ],
+    edges_from_sequence([0,1,2,0,1,2,0])
+)
+
+chord_4g = Knot(
+    [
+        SingularCrossing([8,11],[9,0]),
+        SingularCrossing([0,3],[1,4]),
+        SingularCrossing([4,1],[5,2]),
+        TransverseCrossing(3,6,2,5,True),
+        TransverseCrossing(7,10,6,9,True),
+        SingularCrossing([10,7],[11,8])
+    ],
+    edges_from_sequence([0,1,2,3,1,2,3,4,5,0,4,5,0])
+)
+chord_4d = Knot(
+    [
+        SingularCrossing([9,4],[0,5]),
+        SingularCrossing([7,0],[8,1]),
+        SingularCrossing([1,6],[2,7]),
+        SingularCrossing([5,2],[6,3]),
+        TransverseCrossing(4,9,3,8, True)
+    ],
+    edges_from_sequence([0,1,2,3,4,0,3,2,1,4,0])
+)
+chord_4e = Knot(
+    [
+        SingularCrossing([7,2],[0,3]),
+        SingularCrossing([0,5],[1,6]),
+        SingularCrossing([4,1],[5,2]),
+        SingularCrossing([3,6],[4,7])
+    ],
+    edges_from_sequence([0,1,2,0,3,2,1,3,0])
+)
+chord_4f = Knot(
+    [
+        SingularCrossing([2,11],[3,0]),
+        TransverseCrossing(1,6,0,5, True),
+        SingularCrossing([6,1],[7,2]),
+        TransverseCrossing(4,9,3,8, True),
+        SingularCrossing([9,4],[10,5]),
+        SingularCrossing([7,10],[8,11])
+    ],
+    edges_from_sequence([0,1,2,0,3,4,1,2,5,3,4,5,0])
+)
+chord_4b = Knot(
+    [
+        SingularCrossing([4,9],[5,0]),
+        TransverseCrossing(1,8,0,7,True),
+        SingularCrossing([6,1],[7,2]),
+        SingularCrossing([2,5],[3,6]),
+        SingularCrossing([8,3],[9,4]),
+    ],
+    edges_from_sequence([0,1,2,3,4,0,3,2,1,4,0])
+)
+chord_4c = Knot(
+    [
+        SingularCrossing([9,4],[0,5]),
+        SingularCrossing([5,0],[6,1]),
+        SingularCrossing([1,6],[2,7]),
+        TransverseCrossing(8,3,7,2,True),
+        SingularCrossing([3,8],[4,9])
+    ],
+    edges_from_sequence([0,1,2,3,4,0,1,2,3,4,0])
+)
+
+chord_4a = Knot(
+    [
+        SingularCrossing([4,11],[5,0]),
+        TransverseCrossing(8,1,7,0,True),
+        SingularCrossing([1,6],[2,7]),
+        SingularCrossing([2,9],[3,10]),
+        TransverseCrossing(11,4,10,3,True),
+        SingularCrossing([8,5],[9,6])
+    ],
+    edges_from_sequence([0,1,2,3,4,0,5,2,1,5,3,4,0])
+)
+
 trefoil_reversed = Knot(
     [
         TransverseCrossing(2, 5, 3, 0, False),
@@ -628,4 +744,15 @@ trefoil_reversed = Knot(
         TransverseCrossing(4, 1, 5, 2, False),
     ],
     [(1, 0), (2, 1), (0, 2), (1, 0), (2, 1), (0, 2)],
+)
+
+assignment_knot = Knot(
+    [
+        TransverseCrossing(5, 0, 4, 9, False),
+        TransverseCrossing(1, 6, 0, 5, False),
+        TransverseCrossing(7, 4, 6, 3, False),
+        TransverseCrossing(3,8,2,7, False),
+        TransverseCrossing(9, 2, 8, 1, False),
+    ],
+    edges_from_sequence([0,1,4,3,2,0,1,2,3,4,0])
 )
